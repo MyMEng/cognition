90637cff8b5c543025c0a65d5ac6fc011f8edf22
Kacper <ks1591@bristol.ac.uk>
2015-03-11 00:02:09 +0000

    Workflow framework completed.

--- wire/workflow.sh
+++ wire/workflow.sh
@@ -1,28 +1,36 @@
 #! /bin/bash
 # Workflow
+# {l0,l1,l2,l3,l4,l5,l6,l7,l8,l9}
+ls=(l0 l1 l2 l3 l4 l5 l6 l7 l8 l9)
+# ls=(a8)
 
-mkdir $1
+for i in ${ls[@]}; do
+  mkdir $i
 
-../../SHgen/generator.py ../../SHgen/examples/rooms.l ../../SHgen/examples/layout.l ../../SHgen/examples/activities.l ../../SHgen/examples/path1.ui
+  ../../SHgen/generator.py ../../SHgen/examples/rooms.l ../../SHgen/examples/layout.l ../../SHgen/examples/activities.l ../../SHgen/examples/path1.ui
 
-mv bg.pl $1/
-mv data.txt $1/
-mv data.f $1/
-mv data.n $1/
+  mv bg.pl $i/
+  mv data.txt $i/
+  mv data.f $i/
+  mv data.n $i/
 
-./formatData.py $1/data.txt
-mv data.pl $1/
+  ./formatData.py $i/data.txt
+  mv data.pl $i/
 
-cp alephHeader.pl $1/data.b
+  cp alephHeader.pl $i/data.b
 
-cat $1/bg.pl >> $1/data.b
-cat $1/data.pl >> $1/data.b
+  cat $i/bg.pl >> $i/data.b
+  cat $i/data.pl >> $i/data.b
 
-cp learn.ypl $1/
-cp aleph.yap $1/
+  cp learn.ypl $i/
+  cp aleph.yap $i/
 
-# Copy the directory "foo" from the local host to a remote host's directory "bar"
-scp -r $1 kacper@192.168.56.101:~/
-ssh kacper@192.168.56.101 $1/learn.ypl
-scp kacper@192.168.56.101:~/$1/rules.pl $1/
-ssh kacper@192.168.56.101 rm -fr $1
+  # Copy the directory "foo" from the local host to a remote host's directory "bar"
+  scp -r $i kacper@192.168.56.101:~/
+  ssh kacper@192.168.56.101 $i/learn.ypl
+  scp kacper@192.168.56.101:~/$i/rules.pl $i/
+  ssh kacper@192.168.56.101 rm -fr $i
+done
+
+# do cross validation
+./cross-validate.py "${ls[*]}"
