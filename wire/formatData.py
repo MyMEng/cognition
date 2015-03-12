0a87c043504cb5abaed5a54c6cac1a977437d0e3
kacper <ks1591@bristol.ac.uk>
2015-03-12 23:53:47 +0000

    Generate arff files.

--- wire/formatData.py
+++ wire/formatData.py
@@ -4,12 +4,22 @@
 
 import sys
 import time, datetime
-# from pprint import pprint
+from pprint import pprint
 
 # time window length in microsecond (10^-6): 5 seconds
 WINDOWLENGTH = 5 * 1000000
 
+# Prolog ground truth
 activityRule = "activity"
+
+# Weka ground truth
+atRelation = "@RELATION smartHouse\n"
+atAttribute = "@ATTRIBUTE "
+atributeTF = " {true, false}"
+atributeN  = " numeric"
+atributeD  = " \"yyyy-MM-dd HH:mm:ss.SSS\""
+atClass = "\n@ATTRIBUTE class "
+atData = "\n@DATA"
 
 # Data format: 2008-03-28 13:42:40.467418 M18 ON
 def convertDataEntry( sequenced, line ):
@@ -27,6 +37,9 @@ def convertDataEntry( sequenced, line ):
     stamp += msec
   except:
     stamp = time.mktime(datetime.datetime.strptime( date, "%Y-%m-%d %H:%M:%S" ).timetuple())
+
+  # get date in Weka format
+  dateFormat = datetime.datetime.fromtimestamp(round(stamp, 3)).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
 
   # Convert timestamp to integer
   stamp *= 1000000
@@ -72,7 +85,7 @@ def convertDataEntry( sequenced, line ):
   elif signal == "false":
     uniformSignal = "false"
   elif signal == str(numericValue):
-    uniformSignal = signal
+    uniformSignal = numericValue
   else:
     print "Unrecognised signal: ", signal
     sys.exit(1)
@@ -106,7 +119,7 @@ def convertDataEntry( sequenced, line ):
 
   ##############################################################################
 
-  return (stamp, sensor, uniformSignal, action)
+  return (stamp, sensor, uniformSignal, dateFormat, action)
 
 # Construct sensor knowledge
 def sensor_data(sensorID, sensorStatus, timeType, time):
@@ -135,6 +148,29 @@ def get_window( initTime, currentTime ):
   # get window: 0--WINDOWLENGTH is 0
   return int(diff/WINDOWLENGTH)
 
+# check whether ground truth is multi-label
+def checkLabel(dictionary):
+  onLabels = 0
+  curretnLabel = 'none'
+  for i in dictionary:
+    if dictionary[i]:
+      onLabels += 1
+      curretnLabel = i
+  if onLabels > 1:
+    print "Multi-label sets not supported at the moment!"
+    sys.exit(1)
+  return curretnLabel
+
+# get boolean
+def getBool(s):
+  if s.lower() == 'true':
+    return True
+  elif s.lower() == 'false':
+    return False
+  else:
+    print "unknown Bool type: ", s, "!"
+    sys.exit(1)
+
 if __name__ == '__main__':
   # Check whether file is given as argument
   args = sys.argv
@@ -150,8 +186,8 @@ if __name__ == '__main__':
   with open(args[1], 'r') as f: 
     for i, line in enumerate(f):
       out = convertDataEntry(i, line)
-      data.append( out[0:3] )
-      groundFacts += out[3]
+      data.append( out[0:4] )
+      groundFacts += out[4]
 
   # Get name of file without subdirectories
   slashInd = args[1][::-1].find('/')
@@ -174,16 +210,16 @@ if __name__ == '__main__':
       f.write("\n")
 
       # relative time knowledge
-      rule = sensor_data( e[1].lower(), e[2].lower(), "relative", str(e[0] - init) )
+      rule = sensor_data( e[1].lower(), str(e[2]).lower(), "relative", str(e[0] - init) )
       f.write(rule)
       # absolute time knowledge
-      rule = sensor_data( e[1].lower(), e[2].lower(), "absolute", str(e[0]) )
+      rule = sensor_data( e[1].lower(), str(e[2]).lower(), "absolute", str(e[0]) )
       f.write(rule)
       # absolute time knowledge
-      rule = sensor_data( e[1].lower(), e[2].lower(), "sequence", str(i) )
+      rule = sensor_data( e[1].lower(), str(e[2]).lower(), "sequence", str(i) )
       f.write(rule)
       # windowed time knowledge
-      rule = sensor_data( e[1].lower(), e[2].lower(), "windowed", str(get_window( init, e[0] )) )
+      rule = sensor_data( e[1].lower(), str(e[2]).lower(), "windowed", str(get_window( init, e[0] )) )
       f.write(rule)
 
       f.write("\n")
@@ -197,7 +233,11 @@ if __name__ == '__main__':
     win = get_window( init, groundFacts[i][2][0] )
     groundFacts[i][2].insert(0, rel)
     groundFacts[i][2].append(win)
-  # generate positives and negatives - generate only for *sequence*
+
+  # generate positives and negatives - generate only for *sequence* - PROLOG
+  ## and memorise activity rules for WEKA
+  arffGroundTruth = {}
+  arffGroundFacts = groundFacts[:]
   pos = []
   neg = []
   farEnd = len(data)
@@ -222,6 +262,10 @@ if __name__ == '__main__':
     # use only *sequence*
     beginning = a[0][2][2]
     end = a[1][2][2]
+
+    # memorise beginning and end for WEKA
+    arffGroundTruth[a[0][0]] = ( a[0][2], a[1][2] )
+
     # generate for all the events
     for i in range(beginning):
       neg.append( activityRule + "(" + a[0][0] + ", " + str(i) + ")." )
@@ -242,3 +286,143 @@ if __name__ == '__main__':
     nf.write( '\n'.join(neg) )
     nf.write('\n')
 
+
+  
+  # write ARFF file for Weka
+  ## relative
+  recordRarff = name + ".R.arff"
+  ## absolute
+  recordAarff = name + ".A.arff"
+  ## sequenced
+  recordSarff = name + ".S.arff"
+  ## windowed
+  recordWarff = name + ".W.arff"
+  ## date
+  recordDarff = name + ".D.arff"
+  # prepare data to write & # append time
+  Rarff = [atRelation, atAttribute+"time"+atributeN]
+  Aarff = [atRelation, atAttribute+"time"+atributeN]
+  Sarff = [atRelation, atAttribute+"time"+atributeN]
+  Warff = [atRelation, atAttribute+"time"+atributeN]
+  Darff = [atRelation, atAttribute+"time DATE"+atributeD]
+  # get all sensor names
+  sensorNames = []
+  for e in data:
+    f = e[1].lower()
+    ft = atributeTF if type(e[2])==str else atributeN
+    if (f, ft) not in sensorNames:
+      sensorNames.append( (f, ft) )
+  # append attributes
+  for e in sensorNames:
+    Rarff.append(atAttribute + e[0] + e[1])
+    Aarff.append(atAttribute + e[0] + e[1])
+    Sarff.append(atAttribute + e[0] + e[1])
+    Warff.append(atAttribute + e[0] + e[1])
+    Darff.append(atAttribute + e[0] + e[1])
+  # prepare classes
+  classes = []
+  for e in arffGroundFacts:
+    if e[0] not in classes:
+      classes.append(e[0])
+  classesArff = "{"
+  for e in classes:
+    classesArff += e + ','
+  classesArff += 'none}'
+  # append target class attribute
+  Rarff.append(atClass + classesArff)
+  Aarff.append(atClass + classesArff)
+  Sarff.append(atClass + classesArff)
+  Warff.append(atClass + classesArff)
+  Darff.append(atClass + classesArff)
+  # include DATA marker
+  Rarff.append(atData)
+  Aarff.append(atData)
+  Sarff.append(atData)
+  Warff.append(atData)
+  Darff.append(atData)
+  # generate data
+  ## keep track of current sensors status to generate data
+  sensorStatus = {}
+  for e in sensorNames:
+    sensorStatus[e[0]] = False
+  ## keep track of current activity to mark ground truth
+  groundTruth = {}
+  for e in classes:
+    groundTruth[e] = False
+
+
+
+
+
+
+
+
+  # create common data type
+  cdt = []
+  for d in data:
+    if cdt == []:
+      cdt.append( (d[0], d[3], [(d[1], d[2])]) )
+    elif d[0] in cdt[-1]:
+      cdt[-1][-1].append( (d[1], d[2]) )
+    else:
+      cdt.append( (d[0], d[3], [(d[1], d[2])]) )
+  pprint(data)
+  pprint(cdt)
+  ## generate data
+  i = 0
+  for e in data:
+    # update sensor status based on current entry
+    if type(e[2]) == str:
+      if e[2].lower() != 'true' and e[2].lower() != 'false':
+        print "Unknown sensor status (true/false): *", e[2], "*!"
+        sys.exit(1)
+      sensorStatus[e[1].lower()] = getBool(e[2])
+    elif type(e[2]) == float:
+      sensorStatus[e[1].lower()] = e[2]
+    else:
+      print "Unknown sensor status type: *", e[2], "*!"
+      sys.exit(1)
+    racwd = ""
+    for j in sensorNames:
+      racwd += str(sensorStatus[j[0]]).lower() + ','
+    # update label record
+    for j in groundTruth:
+      beg = arffGroundTruth[j][0][2]
+      end = arffGroundTruth[j][1][2]
+      if i in range(beg, end):
+        groundTruth[j] = True
+      else:
+        groundTruth[j] = False
+    # check current class if none give 'none' # detect multi-label issue and report it
+    cc = checkLabel(groundTruth)
+    # remember that time/date goes first
+    Rarff.append(str(e[0] - init) + "," + racwd + cc)
+    Aarff.append(str(e[0]) + ',' + racwd + cc)
+    Sarff.append(str(i) + ',' + racwd + cc)
+    Warff.append(str(get_window( init, e[0] )) + ',' + racwd + cc)
+    Darff.append("\"" + e[3] + "\"," + racwd + cc)
+    # update i
+    i += 1
+
+
+
+
+
+
+
+  # write the files
+  with open(recordRarff, 'wb') as pf:
+    pf.write( '\n'.join(Rarff) )
+    pf.write('\n')
+  with open(recordAarff, 'wb') as pf:
+    pf.write( '\n'.join(Aarff) )
+    pf.write('\n')
+  with open(recordSarff, 'wb') as pf:
+    pf.write( '\n'.join(Sarff) )
+    pf.write('\n')
+  with open(recordWarff, 'wb') as pf:
+    pf.write( '\n'.join(Warff) )
+    pf.write('\n')
+  with open(recordDarff, 'wb') as pf:
+    pf.write( '\n'.join(Darff) )
+    pf.write('\n')
